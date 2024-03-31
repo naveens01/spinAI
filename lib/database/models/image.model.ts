e19ad8e31445ace9adbbbc953197db594d15db62
naveens01 <naveens.ee20@bitsathy.ac.in>
2024-03-31 10:28:03 +0530

    Db and ModelSetup

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,80 @@
+import { Schema, model, models } from "mongoose";
+
+
+
+export interface IImage extends Document {
+    title: string;
+    TransformationType: string;
+    publicId: string;
+    secureUrl: string;
+    width?: number;
+    height?: number;
+    config?: Record<string, any>;
+    transformationUrl?: string;
+    aspectRatio?: string;
+    color?: string;
+    prompt?: string;
+    author: {
+        _id: string;
+        firstName: string;
+        LastName: string;
+    }
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const ImageSchema= new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    TransformationType: {
+        type: String,
+        required: true
+    },
+    publicId: {
+        type: String,
+        required: true
+    },
+    secureUrl: {
+        type: URL,
+        required: true
+    },
+    width: {
+        type: Number
+    },
+    height:{
+        type: Number
+    },
+    config: {
+        type: Object
+    },
+    transformationUrl:{
+        type: URL
+    },
+    aspectRatio:{
+        type: String
+    },
+    color:{
+        type: String
+    },
+    prompt:{
+        type: String
+    },
+    author:{
+        type: Schema.Types.ObjectId, ref: 'User'
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    UpdatedAt:{
+        type: Date,
+        default: Date.now
+    },
+});
+
+
+const Image = models?.Image || model('Image',ImageSchema);
+
+export default Image;       
